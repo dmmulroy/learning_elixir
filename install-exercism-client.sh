528ca19e7ee0c700f003326ee7a1851ba7d3ba7f
Dillon Mulroy <dillon.mulroy@gmail.com>
2017-02-03 13:09:23 -0500

    Fork exercism cli add option to silent output

--- install-exercism-client.sh
+++ install-exercism-client.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-tar -xzvf ./exercism_client/exercism-linux-64bit.tgz -C ./exercism_client/
+tar -xzvf ./exercism_client/exercism.tar.gz -C ./exercism_client/
 rm -rf ~/bin
 mkdir ~/bin
 mv ./exercism_client/exercism ~/bin/
 rm ./exercism_client/exercism
 export PATH=$HOME/bin:$PATH
-exercism configure --key=$EXERCISM_API_KEY
-exercism configure --dir=./exercises
+exercism configure --key=$EXERCISM_API_KEY -s
+exercism configure --dir=./exercises -s
