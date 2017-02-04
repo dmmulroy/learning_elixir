2546d4d5295c462c754b7cf943f8c74cb6e076a3
Dillon Mulroy <dillon.mulroy@gmail.com>
2017-02-04 12:32:05 -0500

    First pass at implementation of Bob.hey/1

--- exercises/elixir/bob/bob.exs
+++ exercises/elixir/bob/bob.exs
@@ -1,8 +1,10 @@
 defmodule Bob do
   def hey(input) do
     cond do
-        true -> raise "Your implementation goes here"
-
+        String.ends_with?(input, "?") -> "Sure."
+        String.replace(input, " ", "") == "" -> "Fine. Be that way!"
+        String.upcase(input) == input && String.match?(input, ~r/\p{Lu}/u) -> "Whoa, chill out!"
+        String.ends_with?(input, "!") || true -> "Whatever."
     end
   end
 end
