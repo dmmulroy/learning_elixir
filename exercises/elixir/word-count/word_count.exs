b491b7dec36bc8d01051621b52d7a08c1f200bd7
Dillon Mulroy <dillon.mulroy@gmail.com>
2017-02-04 14:43:34 -0500

    First implementation of Words.count/1

--- exercises/elixir/word-count/word_count.exs
+++ exercises/elixir/word-count/word_count.exs
@@ -6,6 +6,10 @@ defmodule Words do
   """
   @spec count(String.t) :: map
   def count(sentence) do
-
+    sentence
+    |> String.replace(~r/[^\p{L}\p{N}\p{Z}-]/u, " ")
+    |> String.downcase
+    |> String.split
+    |> Enum.reduce(%{}, &(Map.update(&2, &1, 1, fn(count) -> count + 1 end)))
   end
 end
