454e2aee06eddd4d13533751e9b74dcc990afd58
Dillon Mulroy <dillon.mulroy@gmail.com>
2017-02-04 11:35:32 -0500

    Make a cleaner implementation of SpaceAge.age_on/2

--- exercises/elixir/space-age/space_age.exs
+++ exercises/elixir/space-age/space_age.exs
@@ -8,11 +8,11 @@ defmodule SpaceAge do
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(:earth, seconds), do: seconds / 60 / 60 / 24 / 365.25
-  def age_on(:mercury, seconds), do: seconds / 60 / 60 / 24 / (365.25 * 0.2408467)
-  def age_on(:venus, seconds), do: seconds / 60 / 60 / 24 / (365.25 * 0.61519726)
-  def age_on(:mars, seconds), do: seconds / 60 / 60 / 24 / (365.25 * 1.8808158)
-  def age_on(:jupiter, seconds), do: seconds / 60 / 60 / 24 / (365.25 * 11.862615)
-  def age_on(:saturn, seconds), do: seconds / 60 / 60 / 24 / (365.25 * 29.447498)
-  def age_on(:uranus, seconds), do: seconds / 60 / 60 / 24 / (365.25 * 84.016846)
-  def age_on(:neptune, seconds), do: seconds / 60 / 60 / 24 / (365.25 * 164.79132)
+  def age_on(:mercury, seconds), do: age_on(:earth, seconds) / 0.2408467
+  def age_on(:venus, seconds), do: age_on(:earth, seconds) / 0.61519726
+  def age_on(:mars, seconds), do: age_on(:earth, seconds) / 1.8808158
+  def age_on(:jupiter, seconds), do: age_on(:earth, seconds) / 11.862615
+  def age_on(:saturn, seconds), do: age_on(:earth, seconds) / 29.447498
+  def age_on(:uranus, seconds), do: age_on(:earth, seconds) / 84.016846
+  def age_on(:neptune, seconds), do: age_on(:earth, seconds) / 164.79132
 end
